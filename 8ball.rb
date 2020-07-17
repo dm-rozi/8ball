afce910ccb22a21c528df5acab0d43fdd2158007
dm-rozi <dmitry.rozi@gmail.com>
2020-07-18 00:44:24 +0300

    update comments

--- 8ball.rb
+++ 8ball.rb
@@ -5,28 +5,28 @@ greetings = [
 ]
 
 answers = [
-  # Положительные
+  # Positive
   "It is certain",
   "It is decidedly so",
   "Without a doubt",
   "Yes — definitely",
   "You may rely on it",
 
-  # Нерешительно положительные
+  # Almost positive
   "As I see it, yes",
   "Most likely",
   "Outlook good",
   "Signs point to yes",
   "Yes",
 
-  # Нейтральные
+  # Neutral
   "Reply hazy, try again",
   "Ask again later",
   "Better not tell you now",
   "Cannot predict now",
   "Concentrate and ask again",
 
-  # Отрицательные
+  # Negative
   "Don’t count on it",
   "My reply is no",
   "My sources say no",
